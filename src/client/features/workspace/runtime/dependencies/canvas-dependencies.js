6ab5803cca9b92d81fbb15cd1f1208b5c842c7ec
zy838121851-sys <zy838121851@gmail.com>
2026-07-02 06:32:34 +0800

    refactor: bypass canvas dependency barrel exports

--- src/client/features/workspace/runtime/dependencies/canvas-dependencies.js
+++ src/client/features/workspace/runtime/dependencies/canvas-dependencies.js
@@ -1,60 +1,82 @@
 export {
   addSelectedNodeElement,
-  addSourceBadgeElement,
-  applyTextEditorStyle,
-  bindCanvasMenuActions,
-  bindCanvasRuntimeInfrastructure,
-  buildImageTextEditPrompt,
-  buildPointsPath,
+  clearSelectedNodeElements,
+  replaceSelectedNodeElements
+} from "../../../canvas/canvas-selection.js";
+export {
   centerPanOnWorldPoint,
   clampCanvasZoom,
-  clearSelectedNodeElements,
-  closeOpenImageToolbarMenus,
-  createGenerationPreviewNode,
-  createImageTextPanel,
-  createSelectionBoxElement,
-  createWorkspaceNode,
   DEFAULT_CANVAS_PAN,
-  ensureCanvasNodeId,
-  ensureImageLightbox as ensureImageLightboxElement,
-  findCanvasNodeById,
   fitWorldBoundsInViewport,
-  focusTextEditorAtEnd,
   getCanvasTransformStyle,
-  getElementWorldBounds,
-  getImageTextEdits,
-  getNodeThumbnail,
-  getNodeTitle,
-  getSelectedNodeDeletePayload,
-  getSelectionBoxRect,
-  getTextEditorFromNode,
-  getVisibleCanvasNodes,
-  getWorldSelectionArea,
-  hasShapeNodeInSet,
-  hasTextNodeInSet,
-  hideImageLightboxElement,
-  hideShapeToolbar,
-  hideTextToolbar,
-  initModelViewerPreview,
-  isFixedStrokeToolName,
-  isLinearDrawToolName,
   panForZoomAroundWorldPoint,
-  positionImageTextPanel as positionImageTextPanelElement,
-  positionTextToolbar,
-  recordCanvasEvent as recordCanvasEventToStore,
-  removeCanvasNodeDeep,
-  renderImageTextInputs as renderImageTextInputList,
-  renderNodeTemplate,
-  replacePreviewNodeWithImage,
-  replacePreviewNodeWithModel,
-  replacePreviewNodeWithVideo,
-  replaceSelectedNodeElements,
+  syncZoomControls
+} from "../../../canvas/canvas-viewport.js";
+export {
   setActiveRailButton,
   setActiveRailPanelButton,
-  setTextNodeEditingState,
-  setUploadChoiceHover,
-  showImageLightbox as showImageLightboxElement,
-  syncZoomControls,
   toggleToolRailCollapsed
-} from "../../../canvas/index.js";
-export { SHAPE_TEXT_TOOLS } from "../../../canvas/runtime/index.js";
+} from "../../../canvas/canvas-toolbar.js";
+export { getElementWorldBounds } from "../../../canvas/canvas-geometry.js";
+export { recordCanvasEvent as recordCanvasEventToStore } from "../../../canvas/canvas-events.js";
+export {
+  buildPointsPath,
+  isFixedStrokeToolName,
+  isLinearDrawToolName
+} from "../../../canvas/drawing-tools.js";
+export {
+  buildImageTextEditPrompt,
+  createImageTextPanel,
+  getImageTextEdits,
+  positionImageTextPanel as positionImageTextPanelElement,
+  renderImageTextInputs as renderImageTextInputList
+} from "../../../canvas/image-text-panel.js";
+export { closeOpenImageToolbarMenus } from "../../../canvas/image-toolbar.js";
+export {
+  ensureImageLightbox as ensureImageLightboxElement,
+  hideImageLightboxElement,
+  showImageLightbox as showImageLightboxElement
+} from "../../../canvas/image-lightbox.js";
+export { initModelViewerPreview } from "../../../canvas/model-viewer-loader.js";
+export {
+  createGenerationPreviewNode,
+  createWorkspaceNode,
+  replacePreviewNodeWithImage,
+  replacePreviewNodeWithModel,
+  replacePreviewNodeWithVideo
+} from "../../../canvas/node-creation.js";
+export { ensureCanvasNodeId } from "../../../canvas/node-identity.js";
+export {
+  getSelectedNodeDeletePayload,
+  removeCanvasNodeDeep
+} from "../../../canvas/node-removal.js";
+export {
+  findCanvasNodeById,
+  getNodeThumbnail,
+  getNodeTitle,
+  getVisibleCanvasNodes
+} from "../../../canvas/node-query.js";
+export { renderNodeTemplate } from "../../../canvas/node-template.js";
+export {
+  createSelectionBoxElement,
+  getSelectionBoxRect,
+  getWorldSelectionArea
+} from "../../../canvas/selection-box.js";
+export {
+  hasShapeNodeInSet,
+  hideShapeToolbar
+} from "../../../canvas/shape-tool.js";
+export { addSourceBadgeElement } from "../../../canvas/source-badge.js";
+export {
+  applyTextEditorStyle,
+  focusTextEditorAtEnd,
+  getTextEditorFromNode,
+  hasTextNodeInSet,
+  hideTextToolbar,
+  positionTextToolbar,
+  setTextNodeEditingState
+} from "../../../canvas/text-tool.js";
+export { setUploadChoiceHover } from "../../../canvas/components/upload-choice-bubbles.js";
+export { bindCanvasRuntimeInfrastructure } from "../../../canvas/runtime/canvas-runtime-bindings.js";
+export { SHAPE_TEXT_TOOLS } from "../../../canvas/runtime/director-defaults.js";
+export { bindCanvasMenuActions } from "../../../canvas/workflows/canvas-menu-actions.js";
