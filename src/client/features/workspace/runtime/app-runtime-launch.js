55437617ade50ee139659eb13afb0782cee00080
zy838121851-sys <zy838121851@gmail.com>
2026-07-02 07:20:11 +0800

    refactor: bypass app runtime dependency barrel

--- src/client/features/workspace/runtime/app-runtime-launch.js
+++ src/client/features/workspace/runtime/app-runtime-launch.js
@@ -1,4 +1,5 @@
-import { buildAppRuntimeRefGroups, buildAppRuntimeRefsFromState, launchAppRuntimeFromRuntimeRefs } from "./app-runtime-dependencies.js";
+import { launchAppRuntimeFromRuntimeRefs } from "./app-runtime-bootstrap-dependencies.js";
+import { buildAppRuntimeRefGroups, buildAppRuntimeRefsFromState } from "./app-runtime-ref-builders.js";
 import { bootstrapAppRuntimeFromInputs } from "./app-runtime-host.js?v=20260627-library-bulk-select-1";
 import { buildAppRuntimeInputSources, buildAppRuntimeInputsFromSources } from "./app-runtime-inputs.js";
 
